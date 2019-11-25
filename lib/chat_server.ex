acb0dcd04cd4269050c82db95ebf87782d3d5860
Mirtles <mirte.post@yahoo.co.uk>
2019-11-25 10:09:26 +0100

    Create supervisor module

---
+++ lib/chat_server.ex
@@ -0,0 +1,37 @@
+defmodule ChatServer do
+  use Application
+  @port 4000
+
+  # this defines the supervisor, which will start the application
+  @spec start(any, any) :: {:error, any} | {:ok, pid}
+  def start(_type, _args) do
+    children = [
+      Plug.Cowboy.child_spec(
+        scheme: :http,
+        plug: ChatServer.Router,
+        options: [
+          dispatch: dispatch(),
+          port: @port
+        ]
+      ),
+      Registry.child_spec(
+        keys: :duplicate,
+        name: Registry.ChatServer
+      )
+    ]
+
+    opts = [strategy: :one_for_one, name: ChatServer.Application]
+
+    Supervisor.start_link(children, opts)
+  end
+
+  defp dispatch do
+    [
+      {:_,
+       [
+         {"/ws/[...]", ChatServer.SocketHandler, []},
+         {:_, Plug.Cowboy.Handler, {ChatServer.Router, []}}
+       ]}
+    ]
+  end
+end
