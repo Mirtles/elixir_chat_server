dcb3292d4a049c46de5b6639608fc5dd17440ede
Mirtles <mirte.post@yahoo.co.uk>
2019-11-25 13:27:08 +0100

    Update readme

--- README.md
+++ README.md
@@ -4,18 +4,4 @@ Learning Elixir using [this tutorial](https://medium.com/@loganbbres/elixir-webs
 
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `chat_server` to your list of dependencies in `mix.exs`:
-
-```elixir
-def deps do
-  [
-    {:chat_server, "~> 0.1.0"}
-  ]
-end
-```
-
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at [https://hexdocs.pm/chat_server](https://hexdocs.pm/chat_server).
-
+Works on any computer with Elixir and mix installed. Run ```$ mix deps.get```. Run ```$iex -S mix```. Navigate to http://localhost:4001/ in two different browsers.
--- priv/static/js/application.js
+++ priv/static/js/application.js
@@ -1,11 +1,8 @@
 (() => {
   class myWebsocketHandler {
     setupSocket() {
-      // initialises connection to socket
-      console.log("setting up socket")
       this.socket = new WebSocket("ws://localhost:4001/ws/chat")
 
-      // when a message arrives, a p tag is made and appended to the chat element
       this.socket.addEventListener("message", (event) => {
         const pTag = document.createElement("p")
         pTag.innerHTML = event.data
@@ -13,20 +10,17 @@
         document.getElementById("main").append(pTag)
       })
 
-      // if the websocket is closed, this reopens it
       this.socket.addEventListener("close", () => {
         this.setupSocket()
       })
     }
 
     submit(event) {
-      console.log("submitting")
       event.preventDefault()
       const input = document.getElementById("message")
       const message = input.value
       input.value = ""
 
-      // sends to socket
       this.socket.send(
         JSON.stringify({
           data: { message: message },
